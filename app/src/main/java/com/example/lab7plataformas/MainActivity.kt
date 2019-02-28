764b604a092145f33e665812df562b8881fae382
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2019-02-28 01:37:59 -0600

    Cambios

--- app/src/main/java/com/example/lab7plataformas/MainActivity.kt
+++ app/src/main/java/com/example/lab7plataformas/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.lab7plataformas
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
 import android.widget.Toast
+import com.example.lab7plataformas.adapter.ContactAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.button_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,37 +20,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.button_main)
+
+        recycler.layoutManager=LinearLayoutManager(this,LinearLayout.VERTICAL,false)
+
         //Se inicializa la base de datos
         conexionSQLite = ConexionSQLite(this)
         //Se traen los contactos de la base de datos
         MyApplication.contactArray = conexionSQLite.getContacts
         //Se utiliza adapter para agregar el arreglo al listview
-        var adapter = ArrayAdapter(this, R.layout.listview_item, MyApplication.contactArray)
-        adapter.notifyDataSetChanged()
-        contactList.adapter = adapter
-        //Al seleccionar un contacto se va a la activity de ver usuario
-        contactList.setOnItemClickListener {
-                parent: AdapterView<*>?, view: View?, position: Int, id: Long ->
-            MyApplication.selectedPosition = position
-            val intent: Intent = Intent(  this, Main3Activity::class.java)
-            startActivity(intent)
-            finish()
-        }
-        //Al dejar presionado un elemento de la lista se borra de base de datos
-        contactList.onItemLongClickListener = object: AdapterView.OnItemLongClickListener{
-            override fun onItemLongClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long): Boolean {
-                conexionSQLite.deleteContact(MyApplication.contactArray[position])
-                //Se actualizan los datos de los contactos
-                MyApplication.contactArray = conexionSQLite.getContacts
-                var adapter = ArrayAdapter(applicationContext, R.layout.contact_item, MyApplication.contactArray)
-                contactList.adapter = adapter
-                Toast.makeText(applicationContext, "Se ha eliminado el contacto", Toast.LENGTH_SHORT).show()
-                return true
-            }
-        }
+        var adapter = ContactAdapter(this, MyApplication.contactArray)
+        recycler.adapter = adapter
         //Esta funcion va a la activity de crear un usuario
-        button.setOnClickListener {
+        add.setOnClickListener {
 
             val intent: Intent = Intent(  this, Main2Activity::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/lab7plataformas/adapter/ContactAdapter.kt
+++ app/src/main/java/com/example/lab7plataformas/adapter/ContactAdapter.kt
@@ -1,42 +1,66 @@
 package com.example.lab7plataformas.adapter
 
 import android.content.Context
+import android.content.Intent
+import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListAdapter
-import android.widget.TextView
-import com.example.lab7plataformas.R
-import com.example.lab7plataformas.data.Contact
+import android.widget.*
+import com.bumptech.glide.Glide
+import com.example.lab7plataformas.*
 import kotlinx.android.synthetic.main.contact_item.view.*
 
-class ContactAdapter internal constructor(
-    context: Context
-) : RecyclerView.Adapter<ContactAdapter.WordViewHolder>() {
+class ContactAdapter (var context: Context,var list: ArrayList<Contact>): RecyclerView.Adapter<ContactAdapter.ViewHolder>(){
 
-    private val inflater: LayoutInflater = LayoutInflater.from(context)
-    private var contacts = emptyList<Contact>() // Cached copy of words
+    var myContext: Context = context
 
-    inner class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val contactItemView: TextView = itemView.findViewById(R.id.textView)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v=LayoutInflater.from(parent.context).inflate(R.layout.contact_item,parent,false)
+        return ViewHolder(v)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
-        val itemView = inflater.inflate(R.layout.contact_item, parent, false)
-        return WordViewHolder(itemView)
+    override fun getItemCount(): Int {
+        return list.size
     }
 
-    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
-        val current = contacts[position]
-        holder.contactItemView.text = current.name
+    override fun onBindViewHolder(holder: ContactAdapter.ViewHolder, position: Int) {
+        holder.bindItems(list[position])
+        holder.itemView.setOnClickListener {
+            MyApplication.selectedPosition = position
+            val intent: Intent = Intent(  myContext, Main3Activity::class.java)
+            myContext.startActivity(intent)
+        }
+
+        holder.itemView.setOnLongClickListener(object: AdapterView.OnItemLongClickListener, View.OnLongClickListener {
+            override fun onLongClick(v: View?): Boolean {
+                var conexionSQLite = ConexionSQLite(myContext)
+                conexionSQLite.deleteContact(MyApplication.contactArray[position])
+                //Se actualizan los datos de los contactos
+                MyApplication.contactArray = conexionSQLite.getContacts
+                var adapter = ArrayAdapter(myContext, R.layout.contact_item, MyApplication.contactArray)
+
+                Toast.makeText(myContext, "Se ha eliminado el contacto", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onItemLongClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long): Boolean {
+                return true
+            }
+        })
     }
 
-    internal fun setWords(contacts: List<Contact>) {
-        this.contacts = contacts
-        notifyDataSetChanged()
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+        fun bindItems(data: Contact){
+            val title:TextView=itemView.findViewById(R.id.text_view_title)
+            val description:TextView=itemView.findViewById(R.id.text_view_description)
+
+            title.text=data.name
+            description.text=data.phone
+        }
     }
 
-    override fun getItemCount() = contacts.size
 }
